cd9002b87cdae3972a90edcbce2777945cd40b89
Nazar Otuncheiv <notunchiev@psystems.biz>
2021-04-22 09:33:25 +0200

    feat(main) : added first layout and logic

--- public/script.js
+++ public/script.js
@@ -1,1 +1,66 @@
-console.log('It works.');
+const PRICE = 9.99;
+
+new Vue({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [
+      { id: 1, title: 'item 1' },
+      { id: 2, title: 'item 2' },
+      { id: 3, title: 'item 3' },
+    ],
+    cart: [],
+  },
+  methods: {
+    addItem (index) {
+      this.total += PRICE;
+      let item = this.items[index];
+      let found = false;
+      for (let i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].id === item.id) {
+          found = true;
+          this.cart[i].qty++;
+        }
+      }
+      if (!found) {
+        this.cart.push({
+          id: item.id,
+          title: item.title,
+          qty: 1,
+          price: PRICE,
+        });
+      }
+    },
+    inc (item) {
+      item.qty++;
+      this.total += PRICE;
+    },
+    dec (item) {
+        item.qty--;
+        this.total -= PRICE;
+      if (item.qty <= 0) {
+        for (let i = 0; i < this.cart.length; i++) {
+          if (this.cart[i].id === item.id) {
+            this.cart.splice(i,1);
+            break;
+          }
+        }
+
+      }
+
+
+      this.cart.pop({
+        id: item.id,
+        title: item.title,
+        qty: 1,
+        price: PRICE,
+      })
+    },
+
+  },
+  filters: {
+    currency (price) {
+      return '$'.concat(price.toFixed(2));
+    },
+  },
+});
