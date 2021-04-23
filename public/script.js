8de17f3af3dec2975675b641e6abcbfe2662ea3c
Nazar Otuncheiv <notunchiev@psystems.biz>
2021-04-23 10:53:21 +0200

    feat(scrollload) : added scroll load

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const PRICE = 9.99;
+const LOAD_NUM = 10;
 
 new Vue({
   el: '#app',
@@ -6,16 +7,29 @@ new Vue({
     total: 0,
     items: [],
     cart: [],
-    search: '',
+    results: [],
+    search: 'Malinois',
     lastSearch: '',
+    loading: false,
+    price: 9.99
   },
   methods: {
+    appendItems(){
+      if (this.items.length < this.results.length){
+        let append = this.results.slice(this.items.length, this.items.length + LOAD_NUM);
+        this.items = this.items.concat(append);
+      }
+    },
     onSubmit(){
+      this.items = [];
+      this.loading = true;
       this.$http.get('/search/'.concat(this.search))
         .then(function(res) {
           console.log(res.data)
           this.lastSearch = this.search;
-          this.items = res.data
+          this.results = res.data;
+          this.appendItems();
+          this.loading = false;
       });
     },
     addItem (index) {
@@ -62,4 +76,16 @@ new Vue({
       return '$'.concat(price.toFixed(2));
     },
   },
+  mounted(){
+    this.onSubmit();
+
+    let vueInstance = this;
+    let elem = document.getElementById('product-list-bottom');
+    let watcher = scrollMonitor.create(elem);
+    watcher.enterViewport(function() {
+      vueInstance.appendItems();
+    });
+  }
 });
+
+
